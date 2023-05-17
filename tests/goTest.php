6e89b901dd56ef2dd51f15c8148845bd566e9b4c
zech <nichaschang@gmail.com>
2023-05-18 01:34:13 +0800

    master : master : 新增 test

---
+++ tests/goTest.php
@@ -0,0 +1,9 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class goTest extends TestCase {
+    public function testAddition() {
+        $result = 4;
+        $this->assertEquals(4, $result);
+    }
+}
